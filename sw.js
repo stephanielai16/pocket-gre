178467b8d03c06b6d698b958bc13d60379070de8
stephanielai16 <laistephanie16@gmail.com>
2026-08-01 17:01:33 +0000

    Publish Pocket GRE app

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "pocket-gre-v3";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.add("./")));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response.ok) {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match("./"))));
+});
